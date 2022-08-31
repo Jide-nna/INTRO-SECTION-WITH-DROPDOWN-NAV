1aff8c299256b59a333d99c6525e37802e105f7a
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2022-08-31 22:12:47 +0100

    Jide-nna's work

--- JS/script.js
+++ JS/script.js
@@ -8,19 +8,13 @@ const origins = document.querySelector(".img");
 const original = document.querySelector(".image-e");
 const img = document.querySelector(".togger");
 const image = document.querySelector(".tog");
-/* features.addEventListener('click', function () {
-	featuresMenu.classList.toggle('is-active')
-	companyMenu.classList.toogle('company')
-}); */
 features.addEventListener("click", function () {
   featuresMenu.classList.toggle("act");
-  //companyMenu.classList.toggle('company')
   origins.classList.toggle("togger");
   img.classList.toggle("togger");
 });
 company.addEventListener("click", function () {
   companyMenu.classList.toggle("activer");
-  //featuresMenu.classList.toggle('features')
   original.classList.toggle("tog");
   image.classList.toggle("tog");
 });
@@ -29,7 +23,6 @@ company.addEventListener("click", function () {
 const hamburger = document.querySelector(".mobile-menu");
 const mobileMenu = document.querySelector(".mobile");
 const closeMenu = document.querySelector(".close");
-const closeE = document.querySelector(".e");
 const featuresBtn = document.querySelector(".features-btn-mobile");
 const companyBtn = document.querySelector(".company-btn-mobile");
 const arrowDown = document.querySelector(".mobile-down");
@@ -40,14 +33,15 @@ const mobileFeatures = document.querySelector(".features-mobile");
 const mobileCompany = document.querySelector(".company-mobile");
 
 hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("mobile-menu");
-  // closeMenu.classList.toggle("close");
+  hamburger.classList.toggle("in-active");
+  closeMenu.classList.toggle("mobile-menu");
   mobileMenu.classList.toggle("in-active");
 });
-/* closeMenu.addEventListener("click", function () {
+closeMenu.addEventListener("click", function () {
   closeMenu.classList.toggle("mobile-menu");
   hamburger.classList.toggle("mobile-menu");
-}); */
+  mobileMenu.classList.toggle("in-active");
+});
 featuresBtn.addEventListener("click", function () {
   arrowDown.classList.toggle("mobile-up");
   arrowUp.classList.toggle("mobile-up");
